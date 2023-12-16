772b45c9a8008f6855ceccb6b0baec15dd0238ef
As554 <as554@njit.edu>
2023-12-16 00:16:30 -0500

    issue 39: Fixed app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
+// pages/_app.js
+
 import React from 'react';
 import { NextUIProvider } from '@nextui-org/react';
-import theme from '../styles/Home.module.css'; // Adjust the path as necessary
+import theme from '../styles/Home.module.css'; // This imports your home module styles
+import '../styles/globals.css'; // This imports your global styles
 
 function MyApp({ Component, pageProps }) {
   return (
     <NextUIProvider theme={theme}>
+      {/* The global styles will be applied here, including your green background */}
       <Component {...pageProps} />
     </NextUIProvider>
   );
