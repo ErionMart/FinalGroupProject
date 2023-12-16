0d8aee5652c635636801ec2b1b0f285054f46993
As554 <as554@njit.edu>
2023-12-16 02:52:33 -0500

    issue 43: updated _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,14 @@
 // pages/_app.js
-
 import React from 'react';
 import { NextUIProvider } from '@nextui-org/react';
-import theme from '../styles/Home.module.css'; // This imports your home module styles
-import '../styles/globals.css'; // This imports your global styles
+import '../styles/globals.css';
+import CookieConsent from '../components/CookieConsent'; // Import the component
 
 function MyApp({ Component, pageProps }) {
   return (
-    <NextUIProvider theme={theme}>
-      {/* The global styles will be applied here, including your green background */}
+    <NextUIProvider> {/* Removed the theme prop */}
       <Component {...pageProps} />
+      <CookieConsent /> {/* Add the Cookie Consent component */}
     </NextUIProvider>
   );
 }
