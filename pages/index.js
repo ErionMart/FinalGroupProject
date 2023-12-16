04454b784d219ede857fee41228f5722f23ff2cf
As554 <as554@njit.edu>
2023-12-15 22:26:08 -0500

    issue 20: updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from '../components/NavigationBar.js';
 import HeroSection from '../components/HeroSection';
+import StylesSection from '../components/StylesSection';
 
 const Home = () => {
   return (
     <>
       <NavBar />
       <HeroSection />
+      <StylesSection />
       {/* The rest of your page content */}
     </>
   );
