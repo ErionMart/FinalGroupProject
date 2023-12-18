bb7cfc2289bf89df35c45cdc88ccb29371378eeb
As554 <as554@njit.edu>
2023-12-18 02:12:49 -0500

    issue 56: media qu

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,36 +1,41 @@
 // components/NavigationBar.js
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import styles from '../styles/NavigationBar.module.css';
 
 const NavBar = () => {
   const router = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  // Function to scroll to the desired section
   const scrollToSection = (sectionId) => {
-    // Use the push method with the hash to scroll into view
+    setIsMenuOpen(false); // Close the menu when a link is clicked
     router.push(`/#${sectionId}`);
   };
 
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbarContainer}>
-        {/* Changed to a div and added an onClick event to scroll to the top of the page */}
         <div className={styles.navbarLogo} onClick={() => scrollToSection('top')}>
           TRAILSTRIDE
         </div>
-        <div className={styles.navbarMenu}>
-          {/* Changed to divs and added onClick events to scroll to the respective sections */}
-          <div className={styles.navbarLink} onClick={() => scrollToSection('styles')}>
-            Styles/Shoes
-          </div>
-          <div className={styles.navbarLink} onClick={() => scrollToSection('about')}>
-            About Us
-          </div>
-          <div className={styles.navbarLink} onClick={() => scrollToSection('subscribe')}>
-            Subscribe
-          </div>
+        <div className={styles.menuToggle} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          {/* Icon for the menu toggle */}
+          ☰
         </div>
+        {/* Conditionally render the menu based on isMenuOpen state */}
+        {isMenuOpen && (
+          <div className={styles.navbarMenu}>
+            <div className={styles.navbarLink} onClick={() => scrollToSection('styles')}>
+              Styles/Shoes
+            </div>
+            <div className={styles.navbarLink} onClick={() => scrollToSection('about')}>
+              About Us
+            </div>
+            <div className={styles.navbarLink} onClick={() => scrollToSection('subscribe')}>
+              Subscribe
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
