140038c86b6c0edcc3fea3dc75cc66020b1c1c5a
As554 <as554@njit.edu>
2023-12-18 00:53:55 -0500

    d

--- pages/document.js
+++ pages/document.js
@@ -6,12 +6,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Existing tags */}
+          {/* Other head content like meta tags */}
           
-          {/* Mailchimp script */}
-          <script id="mcjs" dangerouslySetInnerHTML={{
-            __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/403519e955fd5b9c1fb8b371f/e1e6bb3650960af5aad9276a5.js");`
-          }}></script>
+          {/* Mailchimp popup script */}
+          <script id="mcjs" dangerouslySetInnerHTML={{ __html: `
+            !function(c,h,i,m,p){
+              m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)
+            }(document,"script","https://chimpstatic.com/mcjs-connected/js/users/403519e955fd5b9c1fb8b371f/e1e6bb3650960af5aad9276a5.js");
+          ` }}></script>
         </Head>
         <body>
           <Main />
